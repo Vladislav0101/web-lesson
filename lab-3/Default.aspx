0f6f8297bbcd0b45702ca06634ab1000332ecc36
Vladislav <you@example.com>
2021-12-06 10:40:34 +0300

    с

---
+++ lab-3/Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Balmer peak</div>
+        Weight:<asp:TextBox ID="tb_weight" runat="server" style="margin-bottom: 0px"></asp:TextBox>
+        <asp:Button ID="Btn_calc" runat="server" Text="Button" OnClick="Btn_calc_Click" />
+        <p>
+            <asp:Label ID="Lb_result1" runat="server" Text="cognac"></asp:Label>
+        </p>
+        <asp:Label ID="Lb_result2" runat="server" Text="port wine"></asp:Label>
+        <p>
+            <asp:Label ID="Lb_result3" runat="server" Text="dry wine"></asp:Label>
+        </p>
+        <p>
+            <asp:Label ID="Lb_result4" runat="server" Text="Beer"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ lab-3/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {}
+
+    protected void Btn_calc_Click(object sender, EventArgs e)
+    {
+        float weight = float.Parse(tb_weight.Text);
+        float result = weight * 1.35f * 0.7f;
+        Lb_result1.Text = "Cognac " + Math.Round(result / 0.4f) + "   gr.   " ;
+        float hour = weight * 0.15f * 0.7f;
+        Lb_result1.Text += "<i>" + Math.Round(hour / 0.4F) + "  gr./h. " + "</i>";
+        Lb_result2.Text = "Port wine " + Math.Round(result / 0.19f) + "   gr.   ";
+        Lb_result2.Text += "<i>" + Math.Round(hour / 0.19F) + "  gr./h. " + "</i>";
+        Lb_result3.Text = "Dry wine " + Math.Round(result / 0.12f) + "   gr.   ";
+        Lb_result3.Text += "<i>" + Math.Round(hour / 0.12F) + "  gr./h. " + "</i>";
+        Lb_result4.Text = "Beer " + Math.Round(result / 0.06f) + "   gr.   ";
+        Lb_result4.Text += "<i>" + Math.Round(hour / 0.06F) + "  gr./h. " + "</i>";
+    }
+}
